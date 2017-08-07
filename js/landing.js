1c84fa4f5a84f9dfe6b1bded3cb5181747d7c575
stefanobkk <stefano.cassiobkk@gmail.com>
2017-08-07 09:06:50 +0700

    landing page_includes bugs, No products page

---
+++ js/landing.js
@@ -0,0 +1,35 @@
+// jQuery to collapse the navbar on scroll
+function collapseNavbar() {
+    if ($(".navbar").offset().top > 50) {
+        $(".navbar-fixed-top").addClass("top-nav-collapse");
+    } else {
+        $(".navbar-fixed-top").removeClass("top-nav-collapse");
+    }
+}
+
+$(window).scroll(collapseNavbar);
+$(document).ready(collapseNavbar);
+
+// Closes the Responsive Menu on Menu Item Click
+$('.navbar-collapse ul li a').click(function() {
+    $(".navbar-collapse").collapse('hide');
+});
+
+// jQuery for page scrolling feature - requires jQuery Easing plugin
+$(function() {
+    $('a.page-scroll').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1500, 'easeInOutExpo');
+        event.preventDefault();
+    });
+});
+
+
+window.show = function show(elementId){
+    document.getElementById("ima-imgs").style.display="none";
+    document.getElementById("acmi-imgs").style.display="none";
+    document.getElementById("zacmi-imgs").style.display="none";
+    document.getElementById(elementId).style.display="inline";
+}
